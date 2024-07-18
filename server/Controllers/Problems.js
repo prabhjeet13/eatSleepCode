a0a91ab222b6bb43e1ca96546800ca41fe98953b
prabhjeet13 <prabhjeetsingh2000@gmail.com>
2024-07-18 22:06:36 +0530

    new rest apis added related with problem schema.

--- server/Controllers/Problems.js
+++ server/Controllers/Problems.js
@@ -81,3 +81,51 @@ exports.editProblem = async(req,res) => {
         });
     }
 }
+
+exports.getProblemByTag = async(req,res) => {
+
+    try {
+        const {tag} = req.body;
+        const allProblemsByTagWise = await Problems.find({tag}).sort({createdat : -1});
+
+        return res.status(200).json({
+            success : true,
+            message : 'all problems fetch successfully',
+            allProblemsByTagWise,
+        });
+    }catch(error)
+    {
+        return res.status(500).json({
+            success : false,
+            message : 'internal error at problem fetching by tags'
+        })
+    }
+}
+
+exports.getProblemById = async(req,res) => {
+    try {
+
+        const {problemId} = req.body;
+
+        const problemDataById = await Problems.findById({_id : problemId})
+                                .populate("problemCreater")
+                                // .populate("testCases")
+                                // .populate("solutionsSubmittedByCoder")
+                                .exec();
+
+        return res.status(200).json({
+                success : true,
+                message : 'details fetch completed',
+                problemDataById,
+        });                         
+
+
+
+    }catch(error)
+    {
+        return res.status(500).json({
+            success : false,
+            message : 'internal error at problem fetching by id',
+        })   
+    }
+}
--- server/Routes/Problems.js
+++ server/Routes/Problems.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-const {addProblem,editProblem} = require('../Controllers/Problems');
+const {addProblem,editProblem,getProblemByTag,getProblemById} = require('../Controllers/Problems');
 const {auth,isCoder} = require('../Middlewares/Auth');
 
 router.post('/addproblem',auth,isCoder,addProblem);
 router.post('/editproblem',auth,isCoder,editProblem);
+
+
+router.post('/getallproblemsByTagWise',getProblemByTag);
+router.post('/getProblemById',getProblemById);
 module.exports = router;
