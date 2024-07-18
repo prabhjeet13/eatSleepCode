122ef43181f55db0ea496f14283e92c34a809987
prabhjeet13 <prabhjeetsingh2000@gmail.com>
2024-07-18 17:51:44 +0530

    building compiler at a6

---
+++ server/Controllers/executecpp.js
@@ -0,0 +1,33 @@
+
+const fs = require('fs');
+const path = require('path');
+const {v4} = require('uuid');
+
+const outputDirectory = path.join(__dirname,'outputs');
+if(!fs.existsSync(outputDirectory))
+{
+        fs.mkdirSync(outputDirectory,{recursive : true});
+}
+
+
+exports.executecpp = async(filepath) => {
+
+    const jobId = path.basename(filepath).split('.')[0];
+    const output_filename = `${jobId}.exe`;
+    const outPath = path.join(outputDirectory,output_filename);
+
+
+    return new Promise((resolve,reject) => (
+        exec(`g++ ${filepath} -o ${outPath} && cd ${outPath} && .\\${output_filename}`,(error,stdout,stderr) => {
+            if(error)
+            {
+                reject({error,stderr})
+            }
+            if(stderr)
+            {
+                reject(stderr)
+            }
+            resolve(stdout);
+        })
+    ));    
+};
