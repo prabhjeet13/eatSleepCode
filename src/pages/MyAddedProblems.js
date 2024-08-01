71e656f7c6059c77ac81d27d39babb993eea6802
prabhjeet13 <prabhjeetsingh2000@gmail.com>
2024-08-01 18:53:01 +0530

    edit table

--- src/pages/MyAddedProblems.js
+++ src/pages/MyAddedProblems.js
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import { Table , Thead, Tr, Td, Th,Tbody} from 'react-super-responsive-table';
 import 'react-super-responsive-table/dist/SuperResponsiveTableStyle.css';
 import {Link, useNavigate} from 'react-router-dom';
-
+import { FaRegEdit } from "react-icons/fa";
 const MyAddedProblems = () => {
   
   const {user} = useSelector((state) => state.user);
@@ -19,15 +19,15 @@ const MyAddedProblems = () => {
 
 
   return (
-    <div className='md:w-[70%] w-[50%] flex flex-col items-center mt-5 flex-wrap'>
+    <div className='md:w-[70%] w-[50%] flex flex-col items-center mt-5'>
         {
             user.problemsCreated.length > 0 && (
               <Table className = 'mt-5 border-2 border-black flex flex-col ml-10'>
                       <Thead>
-                      <Tr className = 'p-2 text-xl text-black border-2 border-black flex items-center justify-evenly'>
-                          <Th>Problem Name</Th>
-                          <Th>Problem Tag</Th>
-                  
+                      <Tr className = 'p-2 text-xl text-black border-2 border-black flex items-center justify-between'>
+                          <Th>Name</Th>
+                          <Th>Tag</Th>
+                          <Th>Edit</Th>
                       </Tr>
                     </Thead> 
                     <Tbody>
@@ -35,9 +35,10 @@ const MyAddedProblems = () => {
                           user.problemsCreated.map((problem,index) => {
                             return (
                               <Link to = {`/dashboard/problems/editproblem/${problem._id}`}>
-                              <Tr className = 'p-2 flex justify-evenly gap-3 border-2 border-black'>
+                              <Tr className = 'p-2 flex justify-between gap-3 border-2 border-black'>
                                 <Td className = 'text-black font-mono font-bold'>{problem.problemName}</Td>
                                 <Td className = 'text-black font-mono font-bold'> {problem.tag}</Td>
+                                <FaRegEdit />
                               </Tr>
                               </Link>  
                             )
